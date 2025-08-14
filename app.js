dc6164be65c7216452eb8ca7a8aeb2e75b6cfda3
Денис <dae29@tpu.ru>
2025-08-14 23:05:16 +0800

    Доработанный базовый сервер

--- app.js
+++ app.js
@@ -7,4 +7,27 @@ app.get("/", function(request,response){
     response.send("<h1>Привет, Октагон!</h1>");
 });
 
-app.listen(3000);
+app.get("/static",(request, response)=>{
+    res.json({
+        header: "Hello",
+        body : "Octagon NodeJS Test"
+    });
+});
+
+app.get("/dynamic", (request,response)=>{
+    const { a, b, c } = request.query;
+
+    if(!a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c)){
+        return res.json({header: "Error"})
+    }
+
+    const result = (parseFloat(a) * parseFloat(b) * parseFloat(c)) / 3
+
+    res.json({
+        header : "Calculated",
+        body: result.toString()
+    });
+});
+app.listen(3000, () =>{
+    console.log('Сервер запущен на 3000 порту')
+}); 
